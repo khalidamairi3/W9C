24d85a4f4be8be0459258bdb6e6bfa79375f67ac
khalidamairi3 <khalidamairi3@gmail.com>
2020-08-20 20:35:22 -0600

    W9D

--- js/app.js
+++ js/app.js
@@ -17,16 +17,63 @@ var tweets = [
         username: "jose",
         created_at: "01-28-2020",
         age:34
-    }
+    },
+    {
+        message: "message 4",
+        username: "user 1",
+        created_at: "02-28-2020",
+        age:27
+    },
+    {
+        message: "message 5",
+        username: "user 2",
+        created_at: "02-28-2020",
+        age:14
+    },
+    {
+        message: "message 6",
+        username: "user 3",
+        created_at: "02-28-2020",
+        age:22
+    },
+    {
+        message: "message 7",
+        username: "user 4",
+        created_at: "02-28-2020",
+        age:12
+    },
+    {
+        message: "message 8",
+        username: "user 5",
+        created_at: "02-28-2020",
+        age:10
+    },
+    {
+        message: "message 9",
+        username: "user 6",
+        created_at: "02-28-2020",
+        age:19
+    },
+    {
+        message: "message 10",
+        username: "user 7",
+        created_at: "02-28-2020",
+        age:13
+    },
 
 ];
 
 
-tweets.forEach(function (tweet){
-    console.log("message : " + tweet["message"]);
-    console.log("username : " + tweet["username"]);
-    console.log("created_at: " + tweet["created_at"]);
-    console.log("age: " + tweet["age"]);
-    console.log("________________________");
+var adultsTweets = adults_tweets()
 
-});
+console.log(adultsTweets);
+
+function checkAdult(tweet) {
+
+    return (tweet["age"] >= 18);
+
+}
+
+function adults_tweets() {
+   return tweets.filter(checkAdult);
+}
